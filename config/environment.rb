2b0867ae8047d1cba7ee7eed1b4cdc06d5413eb4
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-07-04 16:13:25 +0300

    näkymien suomennusta

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,25 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+else
+  ActionMailer::Base.smtp_settings = {
+      :address        => '',
+      :port           => '',
+      #:authentication => :plain,
+      #:user_name      => ENV['SENDGRID_USERNAME'],
+      #:password       => ENV['SENDGRID_PASSWORD'],
+      #:domain         => 'heroku.com',
+      #:enable_starttls_auto => true
+  }
+end
